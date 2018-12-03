530d6f27d4c993fa78911e2a8eef72ac3f1c42da
Deirdre <deirdre.hegarty@mu.ie>
2018-12-03 23:39:27 +0000

    strategy pattern separate files

---
+++ GeneticAlgorithmConcept/src/Couple.java
@@ -0,0 +1,22 @@
+class Couple implements Crossover{
+
+	Chromosome parentA;
+	Chromosome parentB;
+
+	public Couple(Chromosome i1, Chromosome i2){
+		this.parentA = i1;
+		this.parentB = i2;
+	}
+	public Chromosome [] crossover(){
+		String a1 = parentA.getName().substring(0 , 5);
+		String a2 = parentA.getName().substring(5, parentA.getName().length());
+		String b1 = parentB.getName().substring(0, 5 );
+		String b2 = parentB.getName().substring(5, parentB.getName().length());
+		
+		Chromosome offspring [] = new Chromosome [2];
+		offspring[0] = new Individual(a1+b2);
+		offspring[1] = new Individual(b1+a2);
+
+		return offspring;
+	}
+}
